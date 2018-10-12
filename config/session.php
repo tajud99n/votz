ec72f580fc989eb99803e13daf6b60114f5411ed
tajud99n <giwa208@gmail.com>
2018-10-12 13:25:45 +0100

    session_domain fixed

--- config/session.php
+++ config/session.php
@@ -151,7 +151,7 @@ return [
     |
     */
 
-    'domain' => env('SESSION_DOMAIN', 'votz.com'),
+    'domain' => env('SESSION_DOMAIN', null),
 
     /*
     |--------------------------------------------------------------------------
